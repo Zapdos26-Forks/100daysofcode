213ef0ff80b88a29c9a059931e4d7f71f0650e1a
Zapdos26 <zapdoskid@gmail.com>
2021-01-01 21:47:11 -0500

    Day 1

---
+++ source_code/day001/main.py
@@ -0,0 +1,4 @@
+print("----------------------------\nZap's Band Name Generator\n----------------------------\n")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print(f"Your band name is: {city.title()} {pet.title()}")
