9914922e0d08e86158c8392aa3d63ed24e11ef40
Zapdos26 <zapdoskid@gmail.com>
2021-01-06 22:52:35 -0500

    fix for day 6

--- source_code/day006/main.py
+++ source_code/day006/main.py
@@ -4,10 +4,10 @@ def turn_right():
 
 
 while not at_goal():
-    if wall_on_right():
-        if front_is_clear():
-            move()
-        elif wall_in_front():
+    if front_is_clear():
+        move()
+    elif wall_on_right():
+        if wall_in_front():
             turn_left()
     elif right_is_clear():
         turn_right()
