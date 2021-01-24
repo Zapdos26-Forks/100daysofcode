0d83d383444cf5201d2d8b2d728fe343bcc8ecae
Zapdos26 <zapdoskid@gmail.com>
2021-01-23 23:23:27 -0500

    Day 23

---
+++ source_code/day023/player.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.go_to_start()
+        self.setheading(90)
+
+    def go_up(self):
+        self.forward(10)
+
+    def go_to_start(self):
+        self.goto((0, -280))
+
+    def is_at_finish_line(self):
+        if self.ycor() > 280:
+            return True
+        else:
+            return False
