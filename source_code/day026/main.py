b4d71560c70642a019aac40df5044d0995312036
Zapdos26 <zapdoskid@gmail.com>
2021-01-26 23:49:29 -0500

    Day 26

---
+++ source_code/day026/main.py
@@ -0,0 +1,10 @@
+
+import pandas
+
+if __name__ == "__main__":
+    data = pandas.read_csv("nato_phonetic_alphabet.csv")
+    phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+    print(phonetic_dict)
+    word = input("Enter a word: ").upper()
+    output_list = [phonetic_dict[letter] for letter in word]
+    print(output_list)
